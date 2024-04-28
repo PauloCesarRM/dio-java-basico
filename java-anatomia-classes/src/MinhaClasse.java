0e094a1450dd0dca049a322b2ef0dd61c15e046a
PauloCesarRM <paulocesar100limites@gmail.com>
2024-04-27 23:17:03 -0300

    aula 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,28 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.print("Olá pessoal, sejam bem-vindos");
+        //String meuNome = "Paulo";
+        //tipo, nome e valor correspondente
+
+        String  primeiroNome = "Paulo";
+        String  segundoNome = "Martins";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
     }
+
+//DECLARANDO MÉTODOS        
+//TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
+
+//Todo arquivo .java deve começar com a letra MAIÚSCULA
+//Toda variável deve ser escrita com aletra minúscula. Obedecer a regra camelCase
+
+
