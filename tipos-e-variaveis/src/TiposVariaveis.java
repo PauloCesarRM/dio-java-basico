464d69a43e69f75d8ad397ac9b0f607192d8b0aa
PauloCesarRM <paulocesar100limites@gmail.com>
2024-04-28 21:43:40 -0300

    Tipos e variaveis 2/3/4

---
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //double salarioMinimo = 2500.44;
+        //double salario = 2.5;
+        
+        //EXEMPLO DE USO DE CADA TIPO DE VARIÁVEL
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 355510000;
+        long cpf = 10110110110L;
+        float pi = 3.14F;
+        double saalrio = 1275.33;
+
+        
+    }
+}
